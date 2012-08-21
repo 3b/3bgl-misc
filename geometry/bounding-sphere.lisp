d7123a50ac7fe7e1f04b1c6d7d485841497fdda9
Bart Botta <00003b@gmail.com>
2012-08-21 06:21:14 -0500

    clean up commented junk, fix indentation

--- geometry/bounding-sphere.lisp
+++ geometry/bounding-sphere.lisp
@@ -9,13 +9,8 @@
 ;; implement EPOS approximation from 'Fast and Tight Bounding Spheres', Larsson
 ;; see if vector of 3xN floats is faster than vector of sb-cga:vec?
 
-#++
-(defparameter *updates* 0)
-#++(declaim (type fixnum *updates*))
-
 ;; Welzl move-to-front miniball, simplified a bit for 3d only
 (defun mtf-mb (points &key (end (length points)) (dim 0) p1 p2 p3)
-  #++(when (= dim 0) (setf *updates* 0))
   (multiple-value-bind (r c)
       (%mtf-mb points :end end :dim dim :p1 p1 :p2 p2 :p3 p3)
     (values (sqrt r) c)))
@@ -33,12 +28,12 @@
                (declare (sb-cga:vec p))
                (sb-cga:dot-product p p))
              (mb0 (p)
-                  (declare (sb-cga:vec p))
-                  (setf (aref c 0) (aref p 0)
-                        (aref c 1) (aref p 1)
-                        (aref c 2) (aref p 2))
-                  (setf r 0.0
-                        p1 p))
+               (declare (sb-cga:vec p))
+               (setf (aref c 0) (aref p 0)
+                     (aref c 1) (aref p 1)
+                     (aref c 2) (aref p 2))
+               (setf r 0.0
+                     p1 p))
              (mb1 (p)
                (declare (sb-cga:vec p))
                (let ((d (sb-cga:vec- p p1)))
@@ -57,11 +52,11 @@
                           (sb-cga:vec p1-p2 p2-p3)
                           (single-float d))
                  (when (< (abs d) 0.00001) ;; fixme: make constant configurable
-                 ;; points are too close together, or collinear, not sure
-                 ;; what 'correct' way to handle it it, so for now just
-                 ;; return an empty disc on the assumption it isn't
-                 ;; adding much meaningful info anyway...
-                 (return-from mb2 (values -1.0 (sb-cga:vec 0.0 0.0 0.0))))
+                   ;; points are too close together, or collinear, not sure
+                   ;; what 'correct' way to handle it it, so for now just
+                   ;; return an empty disc on the assumption it isn't
+                   ;; adding much meaningful info anyway...
+                   (return-from mb2 (values -1.0 (sb-cga:vec 0.0 0.0 0.0))))
                  ;; calculate circle through b1,b2,b3
                  (let* ((p2-p1 (sb-cga:vec- p2 p1))
                         (p3-p1 (sb-cga:vec- p3 p1))
@@ -161,7 +156,6 @@
                (d^2 (sb-cga:vec- a b)))
              (mtf (x)
                (declare (fixnum x) (ignorable x))
-               #++(incf *updates*)
                (let ((a (aref points x)))
                  (replace points points :start1 1 :start2 0 :end2 x)
                  (setf (aref points 0) a))))
@@ -198,12 +192,9 @@
                     (mtf i))))
       (values r c))))
 
-#++
-(defparameter *pivots* 0)
 (defun %mtf (points x)
   (declare ((simple-array sb-cga:vec (*)) points))
   (declare (fixnum x) (optimize speed))
-  #++(incf *updates*)
   (let ((a (aref points x)))
     (replace points points :start1 1 :start2 0 :end2 x)
     (setf (aref points 0) a)))
@@ -215,8 +206,6 @@
 (defun mtf-pivot (points &key (max-error 0.0))
   (declare (optimize speed))
   (declare ((simple-array sb-cga:vec (*)) points))
-  #++(setf *pivots* 0
-           *updates* 0)
   (let ((tmp (sb-cga:vec 0.0 0.0 0.0))
         (max-error (float max-error 0.0)))
     (declare (dynamic-extent tmp)
@@ -244,20 +233,20 @@
                             (return-from mtf-pivot
                               (values (sqrt r^2) c)))
                           (return (list best d-best)))
-            do #++(incf *pivots*)
-              (let ((r (sqrt r^2))
-                    (d (sqrt d-best)))
-                (when (< (- d r) (* d max-error))
-                  (return-from mtf-pivot
-                    (values d c))))
-              ;; since we have vector of points instead of lists, optimize
-              ;; move-to-front a bit by swapping with something closer
-              ;; to the front of list first if possible
-              ;; (usually only have a few swaps, but from arbitrary
-              ;;  points, so this way we usually only shift a few
-              ;;  entries instead of O(N/2) entries)
-              (when (> best (+ i 2))
-                (rotatef (aref points (1+ i)) (aref points best))
-                (setf best (1+ i)))
-              (%mtf points best)))))
+            do
+               (let ((r (sqrt r^2))
+                     (d (sqrt d-best)))
+                 (when (< (- d r) (* d max-error))
+                   (return-from mtf-pivot
+                     (values d c))))
+               ;; since we have vector of points instead of lists, optimize
+               ;; move-to-front a bit by swapping with something closer
+               ;; to the front of list first if possible
+               ;; (usually only have a few swaps, but from arbitrary
+               ;;  points, so this way we usually only shift a few
+               ;;  entries instead of O(N/2) entries)
+               (when (> best (+ i 2))
+                 (rotatef (aref points (1+ i)) (aref points best))
+                 (setf best (1+ i)))
+               (%mtf points best)))))
 
